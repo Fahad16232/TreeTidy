fdf6e7cce7ace118b4b5d1c15f7fb7aa5ecbf312
dsv <zxcv>
2024-12-06 06:51:14 +0500

    Initial Commit

---
+++ TreeTidy/TreeTidyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeTidyApp.swift
+//  TreeTidy
+//
+//  Created by Muhammad Rizwan on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TreeTidyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
